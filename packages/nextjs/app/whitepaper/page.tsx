5a7438cd2285dabdca606cb7ba0156498cdb9321
bglobal <55815845+brantleyglobal@users.noreply.github.com>
2026-08-26 15:44:56 -0400

    Add files via upload

---
+++ packages/nextjs/app/whitepaper/page.tsx
@@ -0,0 +1,13 @@
+//"use client";
+import WhitepaperClientWrapper from "~~/components/pages/WhitepaperClientWrapper";
+
+//export const dynamic: "force-dynamic" = "force-dynamic";
+
+export const metadata = {
+  title: "WhitePaper | BG Company",
+  description: "Track your transactions and token balances in real time.",
+};
+
+export default function WhitepaperPage() {
+  return <WhitepaperClientWrapper />;
+}
