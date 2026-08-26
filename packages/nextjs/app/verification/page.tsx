c6f679931b41bb207bcfee7d7b814a2d7ebecafe
bglobal <55815845+brantleyglobal@users.noreply.github.com>
2026-08-26 15:41:02 -0400

    Add files via upload

---
+++ packages/nextjs/app/verification/page.tsx
@@ -0,0 +1,16 @@
+//"use client";
+import VerificationClientWrapper from "~~/components/pages/VerificationClientWrapper";
+
+//export const dynamic: "force-dynamic" = "force-dynamic";
+
+export const metadata = {
+  title: "Verification | BG Company",
+  description: "Get The Right Information To Make Informed Decisions.",
+  icons: {
+    icon: "/favicon.png",
+  },
+};
+
+export default function VerificationPage() {
+  return <VerificationClientWrapper />;
+}
